c04b76978b5f73bed15d5be211399f2ec93e6dd3
Loris Matteri <loris.matteri93@gmail.com>
2020-11-18 16:49:47 +0100

    Lista in ordine alfabetico

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,20 @@
 // Chiedere all'utente il cognome
+var userName = prompt('Inserisci il tuo cognome');
+
+//console.log(userName);
+
 // Inserirlo in un array con altri cognomi:
+var names = ['Bianchi', 'Rossi', 'Ibrahimovic', 'Conte', 'Salvini'];
+
+names.push(userName);
+
 // Stampa la lista ordinata alfabeticamente
+names.sort();
+
+for (var i = 0; i < names.length; i++) {
+  var name = names[i];
+  var listContent = document.getElementById('alfa_list').innerHTML;
+  document.getElementById('alfa_list').innerHTML = listContent + '<li>' + name + '</li>';
+}
+
 // Scrivi anche la posizione "umana" della lista in cui il nuovo utente si trova
