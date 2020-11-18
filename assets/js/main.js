3c496182dbb761be2e0f0b134448eb49ea842375
Loris Matteri <loris.matteri93@gmail.com>
2020-11-18 15:42:48 +0100

    Primo commit

---
+++ assets/js/main.js
@@ -0,0 +1,4 @@
+// Chiedere all'utente il cognome
+// Inserirlo in un array con altri cognomi:
+// Stampa la lista ordinata alfabeticamente
+// Scrivi anche la posizione "umana" della lista in cui il nuovo utente si trova
